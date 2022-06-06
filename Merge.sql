004f6a0f29ef619b00d343a37756145807e9d33f
Abhishek Dasgupta <55708520+Abhishek010397@users.noreply.github.com>
2022-06-07 02:43:34 +0530

    Merge Into Command for External Stage Snowflake Using Snowpipe

---
+++ Merge.sql
@@ -0,0 +1,22 @@
+MERGE INTO STAGING_TABLE USING SOURCE_TABLE
+    ON STAGING_TABLE.ID = SOURCE_TABLE.ID
+    WHEN MATCHED THEN
+        UPDATE SET  STAGING_TABLE.COL1 = SOURCE_TABLE.COL1,
+                    STAGING_TABLE.COL2= SOURCE_TABLE.COL2,
+                    ......................................,
+                    ......................................,
+                    STAGING_TABLE.COLN = SOURCE_TABLE.COLN
+    WHEN NOT MATCHED THEN INSERT (
+        COL1,
+        COL2,
+        ....,
+        ....,
+        COLN
+     ) VALUES (
+        SOURCE_TABLE.COL1,
+        SOURCE_TABLE.COL2,
+        .................,
+        .................,
+        .................,
+        SOURCE_TABLE.COLN,
+     )
